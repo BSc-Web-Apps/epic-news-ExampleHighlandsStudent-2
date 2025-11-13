72ff4d639270f1803b7822dc855b38783e86f254
GedSargent <120935232+GedSargent@users.noreply.github.com>
2025-11-13 10:19:31 +0000

    Add TeamMemberCard component

--- app/root.tsx
+++ app/root.tsx
@@ -6,8 +6,8 @@ import Document from './components/shared-layout/Document.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
 import headshot from '~/assets/jpg/portrait-01.jpg'
-// import headshot2 from '~/assets/jpg/portrait-02.jpg'
-// import headshot3 from '~/assets/jpg/portrait-03.jpg'
+import headshot2 from '~/assets/jpg/portrait-02.jpg'
+import headshot3 from '~/assets/jpg/portrait-03.jpg'
 import { RiTwitterXFill, RiLinkedinBoxFill } from 'react-icons/ri'
 
 export const links: Route.LinksFunction = () => {
@@ -16,16 +16,17 @@ export const links: Route.LinksFunction = () => {
 export { meta } from './__root.client.tsx'
 export { headers, loader } from './__root.server.tsx'
 
-interface TeamMemberProps {
-	imgSrc: string
+interface TeamMemberCardProps {
 	name: string
 	role: string
+	imageSrc: string
 }
-const TeamMemberCard = ({ imgSrc, name, role }: TeamMemberProps) => {
+
+export function TeamMemberCard({ name, role, imageSrc }: TeamMemberCardProps) {
 	return (
 		<div className="w-fit rounded-lg bg-slate-800 p-8">
 			<img
-				src={imgSrc}
+				src={imageSrc}
 				alt="An employee"
 				className="mx-auto h-64 w-64 rounded-full"
 			/>
@@ -53,27 +54,22 @@ export default function App() {
 				<div className="flex-1">
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega">Epic News</h1>
-						<div className="w-fit rounded-lg bg-slate-800 p-8">
-							<img
-								src={headshot}
-								alt="An employee"
-								className="mx-auto h-64 w-64 rounded-full"
-							/>
 
-							<div className="pt-6">
-								<h3 className="font-semi-bold text-center text-white">
-									Leonard Krasner
-								</h3>
-								<p className="pt-1 text-center text-slate-400">
-									Senior Designer
-								</p>
-
-								<div className="flex justify-center gap-4 pt-6 text-slate-400">
-									<RiTwitterXFill />
-									<RiLinkedinBoxFill />
-								</div>
-							</div>
-						</div>
+						<TeamMemberCard
+							name="Leonard Krasner"
+							role="Senior Designer"
+							imageSrc={headshot}
+						/>
+						<TeamMemberCard
+							name="John Smith"
+							role="Lead Developer"
+							imageSrc={headshot2}
+						/>
+						<TeamMemberCard
+							name="Jane Doe"
+							role="Marketing Manager"
+							imageSrc={headshot3}
+						/>
 					</main>
 				</div>
 			</div>
