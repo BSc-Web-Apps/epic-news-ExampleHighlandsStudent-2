187ff2a0273844acf5413990d5b2c31fa7596d34
GedSargent <120935232+GedSargent@users.noreply.github.com>
2025-11-15 16:01:40 +0000

    Add HeaderWithSearch to root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -3,6 +3,8 @@ import { useLoaderData } from 'react-router'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
+import FooterLogoCentre from './components/organisms/Footer/FooterLogoCentre.tsx'
+import HeaderWithSearch from './components/organisms/HeaderWithSearch/index.tsx'
 import Document from './components/shared-layout/Document.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
@@ -54,6 +56,7 @@ export default function App() {
 		<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
+					<HeaderWithSearch />
 					<main className="grid h-full place-items-center">
 						<h1 className="text-mega">Epic News</h1>
 
@@ -80,6 +83,7 @@ export default function App() {
 				<div className="container flex justify-between pb-5">
 					<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
 				</div>
+				<FooterLogoCentre />
 			</div>
 		</Document>
 	)
