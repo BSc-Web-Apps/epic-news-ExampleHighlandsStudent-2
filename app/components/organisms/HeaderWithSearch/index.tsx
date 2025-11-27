548cb7ccd9e18e21cd793681c591cf4a890e0ef1
GedSargent <120935232+GedSargent@users.noreply.github.com>
2025-11-27 11:36:35 +0000

    Added Outlet logic to render child route components

--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -4,7 +4,6 @@ import { Button } from '#app/components/ui/button.tsx'
 import { UserDropdown } from '#app/components/user-dropdown.tsx'
 import { useOptionalUser } from '#app/utils/user.ts'
 import logo from '~/assets/png/epic-news-logo.png'
-import HeroCallToAction from '~/components/organisms/Hero/HeroCallToAction.tsx'
 
 export default function HeaderWithSearch() {
 	const matches = useMatches()
@@ -15,9 +14,26 @@ export default function HeaderWithSearch() {
 	return (
 		<header className="container py-6">
 			<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
-				<div className="flex items-center gap-4">
+				<Link to="/" className="flex items-center gap-4">
 					<img src={logo} alt="Epic News Logo" className="w-16" />
 					<span className="text-foreground text-sm">Epic News</span>
+				</Link>
+
+				<div className="flex flex-1 justify-center gap-8">
+					<Link
+						to="/about-us"
+						prefetch="intent"
+						className="text-muted-foreground hover:text-foreground text-sm font-semibold transition"
+					>
+						About us
+					</Link>
+					<Link
+						to="/contact-us"
+						prefetch="intent"
+						className="text-muted-foreground hover:text-foreground text-sm font-semibold transition"
+					>
+						Contact us
+					</Link>
 				</div>
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
 					{searchBar}
--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
-import { RiTwitterXFill, RiLinkedinBoxFill } from 'react-icons/ri'
-import { useLoaderData } from 'react-router'
+import { Outlet, useLoaderData } from 'react-router'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
@@ -9,44 +8,12 @@ import Document from './components/shared-layout/Document.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
-import headshot from '~/assets/jpg/portrait-01.jpg'
-import headshot2 from '~/assets/jpg/portrait-02.jpg'
-import headshot3 from '~/assets/jpg/portrait-03.jpg'
-import HeroCallToAction from './components/organisms/Hero/HeroCallToAction.tsx'
 
 export const links: Route.LinksFunction = () => {
 	return rootLinkElements
 }
 export { meta } from './__root.client.tsx'
 export { headers, loader } from './__root.server.tsx'
-
-interface TeamMemberCardProps {
-	name: string
-	role: string
-	imageSrc: string
-}
-
-export function TeamMemberCard({ name, role, imageSrc }: TeamMemberCardProps) {
-	return (
-		<div className="w-fit rounded-lg bg-slate-800 p-8">
-			<img
-				src={imageSrc}
-				alt="An employee"
-				className="mx-auto h-64 w-64 rounded-full"
-			/>
-
-			<div className="pt-6">
-				<h3 className="font-semi-bold text-center text-white">{name}</h3>
-				<p className="pt-1 text-center text-slate-400">{role}</p>
-
-				<div className="flex justify-center gap-4 pt-6 text-slate-400">
-					<RiTwitterXFill />
-					<RiLinkedinBoxFill />
-				</div>
-			</div>
-		</div>
-	)
-}
 
 export default function App() {
 	const data = useLoaderData<typeof loader | null>()
@@ -58,41 +25,8 @@ export default function App() {
 			<div className="flex h-screen flex-col justify-between">
 				<div className="flex-1">
 					<HeaderWithSearch />
-					<main className="grid h-full place-items-center">
-						<h1 className="text-mega">Epic News</h1>
 
-						<div className="w-full py-16">
-							<HeroCallToAction
-								image="https://www.helpguide.org/wp-content/uploads/2020/01/Social-Media-and-Mental-Health.webp"
-								imageRight={true}
-							>
-								<div className="flex flex-col gap-8 px-8">
-									<h2 className="text-h2">Welcome to Epic News</h2>
-									<p className="text-lg">
-										Keep up to date with the latest tech news.
-									</p>
-								</div>
-							</HeroCallToAction>
-						</div>
-
-						<div className="m-4 flex gap-4">
-							<TeamMemberCard
-								name="Leonard Krasner"
-								role="Senior Designer"
-								imageSrc={headshot}
-							/>
-							<TeamMemberCard
-								name="John Smith"
-								role="Lead Developer"
-								imageSrc={headshot2}
-							/>
-							<TeamMemberCard
-								name="Jane Doe"
-								role="Marketing Manager"
-								imageSrc={headshot3}
-							/>
-						</div>
-					</main>
+					<Outlet />
 				</div>
 
 				<div className="container flex justify-between pb-5">
