d76f9d046ce827d00dda04c3d1cbd31b5706e30d
GedSargent <120935232+GedSargent@users.noreply.github.com>
2025-12-04 11:20:38 +0000

    Add ArticleCategory model and CSRF setup
    Introduces the ArticleCategory model and its relation to Article in the Prisma schema, migration, and seed data. Adds initial setup for CSRF protection in the login route and wraps the app in AuthenticityTokenProvider. Also integrates EpicToaster for toast notifications.

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,13 @@
 import { Outlet, useLoaderData } from 'react-router'
+import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import FooterLogoCentre from './components/organisms/Footer/FooterLogoCentre.tsx'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch/index.tsx'
 import Document from './components/shared-layout/Document.tsx'
+import { useToast } from './components/toaster.tsx'
+import { EpicToaster } from './components/ui/sonner.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
@@ -19,22 +22,33 @@ export default function App() {
 	const data = useLoaderData<typeof loader | null>()
 	const nonce = useNonce()
 	const theme = useTheme()
+	useToast(data?.toast)
 
 	return (
-		<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
-			<div className="flex h-screen flex-col justify-between">
-				<div className="flex-1">
-					<HeaderWithSearch />
+		<AuthenticityTokenProvider token={data?.csrfToken ?? ''}>
+			<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
+				<div className="flex h-screen flex-col justify-between">
+					<div className="flex-1">
+						<HeaderWithSearch />
 
-					<Outlet />
-				</div>
+						<Outlet />
+					</div>
 
-				<div className="container flex justify-between pb-5">
-					<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
+					<div className="container flex justify-between pb-5">
+						<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
+					</div>
+					<FooterLogoCentre />
 				</div>
-				<FooterLogoCentre />
-			</div>
-		</Document>
+				<EpicToaster
+					closeButton
+					position="bottom-right"
+					theme={theme}
+					expand
+					richColors
+					duration={5000}
+				/>
+			</Document>
+		</AuthenticityTokenProvider>
 	)
 }
 
